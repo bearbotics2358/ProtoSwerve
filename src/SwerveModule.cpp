cd6e2087398465bdb816a38c3cfc1d6b293b6468
Tim Paolella <tim.paolella@gmail.com>
2018-08-04 09:03:00 -0500

    Fixed name on swervemodule's header file, added base code for the source file.

--- src/SwerveModule.cpp
+++ src/SwerveModule.cpp
@@ -1,10 +1,31 @@
-/*
- * SwerveModule.cpp
- *
- *  Created on: Aug 3, 2018
- *      Author: Time
- */
+#include <WPILib.h>
+#include "ctre/Phoenix.h"
+#include <SwerveModule.h>
 
+SwerveModule::SwerveModule(int driveMotorOne, int driveMotorTwo, int turnMotor):
+a_DriveMotorOne(driveMotorOne),
+a_DriveMotorTwo(driveMotorTwo),
+a_TurnMotor(turnMotor)
+{
 
+}
 
+void SwerveModule::UpdateCartesian(float xInput, float yInput)
+{
 
+}
+
+void SwerveModule::UpdatePolar(float radius, float angle)
+{
+
+}
+
+void SwerveModule::UpdateRaw(float driveSpeed, float rotationSpeed)
+{
+
+}
+
+SwerveModule::~SwerveModule(void)
+{
+
+}
