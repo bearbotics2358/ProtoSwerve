020e158cc6dcdb09f1bec502276ded447a50d199
Tim Paolella <tim.paolella@gmail.com>
2018-08-17 19:55:24 -0500

    Mapping counts to absolute angle works, testing mapping absolute angle to  0 through 180 / -180

--- src/SwerveModule.cpp
+++ src/SwerveModule.cpp
@@ -23,11 +23,11 @@ void SwerveModule::UpdatePolar(float radius, float angle)
 
 void SwerveModule::UpdateRaw(float driveSpeed, float rotationSpeed)
 {
-	float scalar = 0.3;
+	float scalar = 0.4;
 
 	a_DriveMotorOne.Set(scalar * driveSpeed); // Because this method is just for
 	a_DriveMotorTwo.Set(scalar * driveSpeed); // mechanically testing the modules,
-	a_TurnMotor.Set(0.2 * rotationSpeed); // I've applied a scalar for safety.
+	a_TurnMotor.Set(scalar * rotationSpeed); // I've applied a scalar for safety.
 }
 
 void SwerveModule::ZeroEncoders(void)
@@ -48,7 +48,15 @@ float SwerveModule::GetAngle(void)
 	float count;
 	count = GetAngleRaw(); // Returns raw value from the encoder
 
-	float ret = ((360/COUNTS_PER_ROTATION) * count);
+	float ret = ((count / COUNTS_PER_ROTATION) * 360); // Rotations * Degrees per rotation
+
+	ret = ((int) ret % 360); // Converts counts to int casts it between 0 and 360 degrees
+
+	if(ret >= 180)
+	{
+		ret = 360 - ret;
+	}
+
 	return ret;
 }
 
