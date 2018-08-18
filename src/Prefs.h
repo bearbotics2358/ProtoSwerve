0a8c984d070b6867555cdabfecbeb7c82f61a3ca
Jason Saab <JSaabR4P70R@gmail.com>
2018-08-17 21:08:46 -0400

    MASSIVE CHANGES (removed 1 empty line) - Jason

--- src/Prefs.h
+++ src/Prefs.h
@@ -9,10 +9,9 @@
 #define JOYSTICK_PORT_ONE 1
 
 #define COUNTS_PER_ROTATION 4096
-#define GEAR_RATIO_SCALAR 1 //Will change when known
+#define GEAR_RATIO_SCALAR 2.7 // Will change when known exactly, Gear Ratio from Drive Motor to Drive Wheel
 
 #define WHEEL_CIRCUM_IN 9.4245
 #define WHEEL_CIRCUM_CM 23.9390
-
 
 #endif
